7818a0ddd9555585b4a30f77774e732ff0369d00
WhiteBlade <whitebladedruid@mac.lan>
2025-12-28 20:56:09 -0800

    Initial Checkin

---
+++ PhotoDateTag/PhotoDateTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoDateTagApp.swift
+//  PhotoDateTag
+//
+//  Created by WhiteBlade on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoDateTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoDateTagTests/PhotoDateTagTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoDateTagTests.swift
+//  PhotoDateTagTests
+//
+//  Created by WhiteBlade on 12/28/25.
+//
+
+import Testing
+@testable import PhotoDateTag
+
+struct PhotoDateTagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
